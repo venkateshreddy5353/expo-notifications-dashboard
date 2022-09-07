c0dc79a998028888927549ac0339a5320fa2dca0
venkatesh reddy <venkatesh.reddy5353@gmail.com>
2022-09-08 01:45:38 +0530

    Update index.ts

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,3 +1,3 @@
-const SERVER_BASE_URL = 'http://localhost:4000/expo-notifications/api/v1';
+const SERVER_BASE_URL = 'https://pushnotificationsapi.meepaisa.com/expo-notifications/api/v1';
 
 export default SERVER_BASE_URL;
